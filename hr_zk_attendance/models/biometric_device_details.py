20ef0fbed58d3d163d963b57df19de7be85799cb
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-12 15:59:08 +0300

    Update biometric_device_details.py

--- hr_zk_attendance/models/biometric_device_details.py
+++ hr_zk_attendance/models/biometric_device_details.py
@@ -2,7 +2,7 @@
 ################################################################################
 #
 #    Cybrosys Technologies Pvt. Ltd.
-#    Copyright (C) 2025-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Copyright (C) 2025-TODAY Cybrosys Technologies (<https://www.cybrosys.com>).
 #    Author: Bhagyadev KP (odoo@cybrosys.com)
 #
 #    This program is free software: you can modify
@@ -26,14 +26,12 @@ from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 
 _logger = logging.getLogger(__name__)
-try:
-    from zk import ZK, const
-except ImportError:
-    _logger.error("Please Install pyzk library.")
 
+# Import ZK and constants from vendored pyzk
+from pyzk.zk import ZK, const
 
 class BiometricDeviceDetails(models.Model):
-    """Model for configuring and connect the biometric device with odoo"""
+    """Model for configuring and connecting the biometric device with Odoo"""
     _name = 'biometric.device.details'
     _description = 'Biometric Device Details'
 
@@ -41,220 +39,141 @@ class BiometricDeviceDetails(models.Model):
     device_ip = fields.Char(string='Device IP', required=True,
                             help='The IP address of the Device')
     port_number = fields.Integer(string='Port Number', required=True,
-                                 help="The Port Number of the Device")
+                                 help='The Port Number of the Device')
     address_id = fields.Many2one('res.partner', string='Working Address',
                                  help='Working address of the partner')
     company_id = fields.Many2one('res.company', string='Company',
-                                 default=lambda
-                                     self: self.env.user.company_id.id,
+                                 default=lambda self: self.env.user.company_id.id,
                                  help='Current Company')
 
-    def device_connect(self, zk):
-        """Function for connecting the device with Odoo"""
+    def _get_connection(self):
+        """Helper to connect to the biometric device"""
+        zk = ZK(self.device_ip,
+                port=self.port_number,
+                timeout=30,
+                password=0,
+                force_udp=False,
+                ommit_ping=False)
         try:
-            conn = zk.connect()
-            return conn
-        except Exception:
-            return False
+            return zk.connect()
+        except Exception as err:
+            raise UserError(_(str(err)))
 
     def action_test_connection(self):
-        """Checking the connection status"""
-        zk = ZK(self.device_ip, port=self.port_number, timeout=30,
-                password=False, ommit_ping=False)
-        try:
-            if zk.connect():
-                return {
-                    'type': 'ir.actions.client',
-                    'tag': 'display_notification',
-                    'params': {
-                        'message': 'Successfully Connected',
-                        'type': 'success',
-                        'sticky': False
-                    }
-                }
-        except Exception as error:
-            raise ValidationError(f'{error}')
+        """Test connectivity to the biometric device"""
+        conn = self._get_connection()
+        conn.disconnect()
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'message': 'Successfully Connected',
+                'type': 'success',
+                'sticky': False,
+            }
+        }
 
     def action_set_timezone(self):
-        """Function to set user's timezone to device"""
-        for info in self:
-            machine_ip = info.device_ip
-            zk_port = info.port_number
-            try:
-                # Connecting with the device with the ip and port provided
-                zk = ZK(machine_ip, port=zk_port, timeout=15,
-                        password=0,
-                        force_udp=False, ommit_ping=False)
-            except NameError:
-                raise UserError(
-                    _("Pyzk module not Found. Please install it"
-                      "with 'pip3 install pyzk'."))
-            conn = self.device_connect(zk)
-            if conn:
-                user_tz = self.env.context.get(
-                    'tz') or self.env.user.tz or 'UTC'
-                user_timezone_time = pytz.utc.localize(fields.Datetime.now())
-                user_timezone_time = user_timezone_time.astimezone(
-                    pytz.timezone(user_tz))
-                conn.set_time(user_timezone_time)
-                return {
-                    'type': 'ir.actions.client',
-                    'tag': 'display_notification',
-                    'params': {
-                        'message': 'Successfully Set the Time',
-                        'type': 'success',
-                        'sticky': False
-                    }
+        """Set device time to current user's timezone"""
+        for rec in self:
+            conn = rec._get_connection()
+            user_tz = (self.env.context.get('tz') or self.env.user.tz or 'UTC')
+            now_utc = pytz.utc.localize(fields.Datetime.now())
+            local_dt = now_utc.astimezone(pytz.timezone(user_tz))
+            conn.set_time(local_dt)
+            conn.disconnect()
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'message': 'Successfully Set the Time',
+                    'type': 'success',
+                    'sticky': False,
                 }
-            else:
-                raise UserError(_(
-                    "Please Check the Connection"))
+            }
 
     def action_clear_attendance(self):
-        """Methode to clear record from the zk.machine.attendance model and
-        from the device"""
-        for info in self:
-            try:
-                machine_ip = info.device_ip
-                zk_port = info.port_number
-                try:
-                    # Connecting with the device
-                    zk = ZK(machine_ip, port=zk_port, timeout=30,
-                            password=0, force_udp=False, ommit_ping=False)
-                except NameError:
-                    raise UserError(_(
-                        "Please install it with 'pip3 install pyzk'."))
-                conn = self.device_connect(zk)
-                if conn:
-                    conn.enable_device()
-                    clear_data = zk.get_attendance()
-                    if clear_data:
-                        # Clearing data in the device
-                        conn.clear_attendance()
-                        # Clearing data from attendance log
-                        self._cr.execute(
-                            """delete from zk_machine_attendance""")
-                        conn.disconnect()
-                    else:
-                        raise UserError(
-                            _('Unable to clear Attendance log.Are you sure '
-                              'attendance log is not empty.'))
-                else:
-                    raise UserError(
-                        _('Unable to connect to Attendance Device. Please use '
-                          'Test Connection button to verify.'))
-            except Exception as error:
-                raise ValidationError(f'{error}')
+        """Clear all attendance logs on the device and locally"""
+        for rec in self:
+            conn = rec._get_connection()
+            conn.enable_device()
+            logs = conn.get_attendance()
+            if not logs:
+                conn.disconnect()
+                raise UserError(_('Attendance log is empty.'))
+            conn.clear_attendance()
+            self._cr.execute('DELETE FROM zk_machine_attendance')
+            conn.disconnect()
 
     @api.model
     def cron_download(self):
-        machines = self.env['biometric.device.details'].search([])
-        for machine in machines:
-            machine.action_download_attendance()
+        """Scheduled job: download attendance from all configured devices"""
+        for device in self.search([]):
+            device.action_download_attendance()
 
     def action_download_attendance(self):
-        """Function to download attendance records from the device"""
-        _logger.info("++++++++++++Cron Executed++++++++++++++++++++++")
-        zk_attendance = self.env['zk.machine.attendance']
-        hr_attendance = self.env['hr.attendance']
-        for info in self:
-            machine_ip = info.device_ip
-            zk_port = info.port_number
-            try:
-                # Connecting with the device with the ip and port provided
-                zk = ZK(machine_ip, port=zk_port, timeout=15,
-                        password=0,
-                        force_udp=False, ommit_ping=False)
-            except NameError:
-                raise UserError(
-                    _("Pyzk module not Found. Please install it"
-                      "with 'pip3 install pyzk'."))
-            conn = self.device_connect(zk)
-            self.action_set_timezone()
-            if conn:
-                conn.disable_device()  # Device Cannot be used during this time.
-                user = conn.get_users()
-                attendance = conn.get_attendance()
-                if attendance:
-                    for each in attendance:
-                        atten_time = each.timestamp
-                        local_tz = pytz.timezone(
-                            self.env.user.partner_id.tz or 'GMT')
-                        local_dt = local_tz.localize(atten_time, is_dst=None)
-                        utc_dt = local_dt.astimezone(pytz.utc)
-                        utc_dt = utc_dt.strftime("%Y-%m-%d %H:%M:%S")
-                        atten_time = datetime.datetime.strptime(
-                            utc_dt, "%Y-%m-%d %H:%M:%S")
-                        atten_time = fields.Datetime.to_string(atten_time)
-                        for uid in user:
-                            if uid.user_id == each.user_id:
-                                get_user_id = self.env['hr.employee'].search(
-                                    [('device_id_num', '=', each.user_id)])
-                                if get_user_id:
-                                    duplicate_atten_ids = zk_attendance.search(
-                                        [('device_id_num', '=', each.user_id),
-                                         ('punching_time', '=', atten_time)])
-                                    if not duplicate_atten_ids:
-                                        zk_attendance.create({
-                                            'employee_id': get_user_id.id,
-                                            'device_id_num': each.user_id,
-                                            'attendance_type': str(each.status),
-                                            'punch_type': str(each.punch),
-                                            'punching_time': atten_time,
-                                            'address_id': info.address_id.id
-                                        })
-                                        att_var = hr_attendance.search([(
-                                            'employee_id', '=', get_user_id.id),
-                                            ('check_out', '=', False)])
-                                        if each.punch == 0:  # check-in
-                                            if not att_var:
-                                                hr_attendance.create({
-                                                    'employee_id':
-                                                        get_user_id.id,
-                                                    'check_in': atten_time
-                                                })
-                                        if each.punch == 1:  # check-out
-                                            if len(att_var) == 1:
-                                                att_var.write({
-                                                    'check_out': atten_time
-                                                })
-                                            else:
-                                                att_var1 = hr_attendance.search(
-                                                    [('employee_id', '=',
-                                                      get_user_id.id)])
-                                                if att_var1:
-                                                    att_var1[-1].write({
-                                                        'check_out': atten_time
-                                                    })
-                                else:
-                                    employee = self.env['hr.employee'].create({
-                                        'device_id_num': each.user_id,
-                                        'name': uid.name
-                                    })
-                                    zk_attendance.create({
-                                        'employee_id': employee.id,
-                                        'device_id_num': each.user_id,
-                                        'attendance_type': str(each.status),
-                                        'punch_type': str(each.punch),
-                                        'punching_time': atten_time,
-                                        'address_id': info.address_id.id
-                                    })
-                                    hr_attendance.create({
-                                        'employee_id': employee.id,
-                                        'check_in': atten_time
-                                    })
-                    conn.disconnect
-                    return True
-                else:
-                    raise UserError(_('Unable to get the attendance log, please'
-                                      'try again later.'))
-            else:
-                raise UserError(_('Unable to connect, please check the'
-                                  'parameters and network connections.'))
+        """Download and import attendance records into Odoo"""
+        zk_logs = self.env['zk.machine.attendance']
+        hr_logs = self.env['hr.attendance']
+        for rec in self:
+            conn = rec._get_connection()
+            rec.action_set_timezone()
+            conn.disable_device()
+            users = conn.get_users()
+            logs = conn.get_attendance()
+            if not logs:
+                conn.disconnect()
+                raise UserError(_('No attendance records found.'))
+            for entry in logs:
+                punch_time = entry.timestamp
+                user_tz = self.env.user.partner_id.tz or 'UTC'
+                local_dt = pytz.timezone(user_tz).localize(punch_time, is_dst=None)
+                utc_dt = local_dt.astimezone(pytz.utc)
+                punch_str = fields.Datetime.to_string(utc_dt)
+                # Find or create employee by device_id_num
+                emp = self.env['hr.employee'].search([
+                    ('device_id_num', '=', entry.user_id)], limit=1
+                )
+                if not emp:
+                    user_info = next((u for u in users if u.user_id == entry.user_id), None)
+                    emp = self.env['hr.employee'].create({
+                        'device_id_num': entry.user_id,
+                        'name': user_info.name if user_info else _('Unknown')
+                    })
+                # Prevent duplicates
+                if not zk_logs.search([
+                        ('device_id_num', '=', entry.user_id),
+                        ('punching_time', '=', punch_str)
+                    ], limit=1):
+                    zk_logs.create({
+                        'employee_id': emp.id,
+                        'device_id_num': entry.user_id,
+                        'attendance_type': str(entry.status),
+                        'punch_type': str(entry.punch),
+                        'punching_time': punch_str,
+                        'address_id': rec.address_id.id,
+                    })
+                # Handle HR attendance record
+                open_log = hr_logs.search([
+                    ('employee_id', '=', emp.id),
+                    ('check_out', '=', False)
+                ], limit=1)
+                if entry.punch == const.CHECK_IN and not open_log:
+                    hr_logs.create({'employee_id': emp.id, 'check_in': punch_str})
+                elif entry.punch == const.CHECK_OUT:
+                    if open_log:
+                        open_log.write({'check_out': punch_str})
+                    else:
+                        last_att = hr_logs.search([
+                            ('employee_id', '=', emp.id)
+                        ], order='check_in desc', limit=1)
+                        if last_att:
+                            last_att.write({'check_out': punch_str})
+            conn.disconnect()
+        return True
 
     def action_restart_device(self):
-        """For restarting the device"""
-        zk = ZK(self.device_ip, port=self.port_number, timeout=15,
-                password=0,
-                force_udp=False, ommit_ping=False)
-        self.device_connect(zk).restart()
+        """Restart the biometric device remotely"""
+        conn = self._get_connection()
+        conn.restart()
+        conn.disconnect()
