0612aaf0c2c7a8e5b961aed43d8b59b1b24c6c63
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-17 01:32:17 +0300

    Update subscription_invoice_wizard.py

--- sub_full_first_invoice/wizard/subscription_invoice_wizard.py
+++ sub_full_first_invoice/wizard/subscription_invoice_wizard.py
@@ -1,19 +1,15 @@
-## 6. `wizard/subscription_invoice_wizard.py`
-```python
 # -*- coding: utf-8 -*-
-from odoo import api, models
+from odoo import api, models, fields
 
 class SubscriptionInvoiceWizard(models.TransientModel):
-    _inherit = 'sale.subscription.invoice'  # adjust if your wizard's model differs
+    _inherit = 'sale.subscription.invoice'  # تأكد أنّ هذا الاسم مطابق في نظامك
 
     @api.model
     def default_get(self, fields_list):
         res = super(SubscriptionInvoiceWizard, self).default_get(fields_list)
-        # if coming from a sale.order, find the related subscription
         order_id = self._context.get('active_id')
         if order_id:
-            sub = self.env['sale.subscription'].search([
-                ('order_id', '=', order_id)], limit=1)
+            sub = self.env['sale.subscription'].search([('order_id', '=', order_id)], limit=1)
             if sub and sub.date_start:
                 first = sub.date_start[:7] + '-01'
                 res.update({
@@ -21,4 +17,3 @@ class SubscriptionInvoiceWizard(models.TransientModel):
                     'next_invoice_date': first,
                 })
         return res
-```
