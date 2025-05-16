c9a7e6210ad87f42796c878522c6e54151b56288
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-17 01:16:27 +0300

    Create __manifest__.py

---
+++ sub_full_first_invoice/__manifest__.py
@@ -0,0 +1,8 @@
+# my_addons/sub_full_first_invoice/__manifest__.py
+{
+    'name': 'First-of-Month First Invoice',
+    'version': '1.0',
+    'depends': ['sale_subscription'],
+    'data': [],
+    'installable': True,
+}
