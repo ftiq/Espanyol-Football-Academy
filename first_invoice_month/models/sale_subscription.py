456250e2211fe8961ecfdbaccbd086271414954c
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-17 04:51:51 +0300

    Update sale_subscription.py

--- first_invoice_month/models/sale_subscription.py
+++ first_invoice_month/models/sale_subscription.py
@@ -1,38 +1,26 @@
 from odoo import api, fields, models
 
-class SaleSubscription(models.Model):
-    _inherit = 'sale.subscription'
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
 
     @api.model
     def create(self, vals):
-        if vals.get('date_start'):
+        # عند إنشاء أمر البيع، اجعل التاريخ لأول الشهر
+        if vals.get('date_order'):
             try:
-                ds = fields.Date.from_string(vals['date_start'])
-                vals['date_start'] = ds.replace(day=1)
+                ds = fields.Date.from_string(vals['date_order'])
+                vals['date_order'] = ds.replace(day=1)
             except Exception:
                 pass
-        subscription = super().create(vals)
-        if subscription.next_invoice_date:
-            try:
-                nd = fields.Date.from_string(subscription.next_invoice_date)
-                subscription.next_invoice_date = nd.replace(day=1)
-            except Exception:
-                pass
-        return subscription
+        return super().create(vals)
 
     def write(self, vals):
         res = super().write(vals)
-        for sub in self:
-            if sub.date_start:
+        for order in self:
+            if order.date_order:
                 try:
-                    ds = fields.Date.from_string(sub.date_start)
-                    sub.date_start = ds.replace(day=1)
-                except Exception:
-                    pass
-            if sub.next_invoice_date:
-                try:
-                    nd = fields.Date.from_string(sub.next_invoice_date)
-                    sub.next_invoice_date = nd.replace(day=1)
+                    ds = fields.Date.from_string(order.date_order)
+                    order.date_order = ds.replace(day=1)
                 except Exception:
                     pass
         return res
