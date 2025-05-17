c9ad3b5e6bc8e5989a064d0ad626b62e2885297e
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-17 04:23:07 +0300

    Update sale_subscription.py

--- first_invoice_month/ models/sale_subscription.py
+++ first_invoice_month/ models/sale_subscription.py
@@ -5,14 +5,13 @@ class SaleSubscription(models.Model):
 
     @api.model
     def create(self, vals):
-        # عدل تاريخ البدء لأول الشهر
+        # اجعل التاريخ لأول الشهر عند إنشاء الاشتراك
         if vals.get('date_start'):
             try:
                 ds = fields.Date.from_string(vals['date_start'])
                 vals['date_start'] = fields.Date.to_string(ds.replace(day=1))
             except Exception:
                 pass
-        # عدل تاريخ الفاتورة التالية لأول الشهر
         if vals.get('next_invoice_date'):
             try:
                 nd = fields.Date.from_string(vals['next_invoice_date'])
@@ -22,7 +21,6 @@ class SaleSubscription(models.Model):
         return super().create(vals)
 
     def write(self, vals):
-        # في حال التعديل أيضاً (معالجة التواريخ في القيم المعدلة فقط)
         if vals.get('date_start'):
             try:
                 ds = fields.Date.from_string(vals['date_start'])
