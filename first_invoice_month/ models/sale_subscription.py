52f4a75dc75b4b1a74f2e2d1fe61367c1fa9c602
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-17 04:12:12 +0300

    Update sale_subscription.py

--- first_invoice_month/ models/sale_subscription.py
+++ first_invoice_month/ models/sale_subscription.py
@@ -1,39 +1,40 @@
-# -*- coding: utf-8 -*-
 from odoo import api, fields, models
-from datetime import datetime
 
 class SaleSubscription(models.Model):
     _inherit = 'sale.subscription'
 
     @api.model
     def create(self, vals):
-        # تأكد أن date_start و next_invoice_date في أول الشهر
+        # اجعل التاريخ لأول الشهر عند إنشاء الاشتراك
         if vals.get('date_start'):
             try:
                 ds = fields.Date.from_string(vals['date_start'])
                 vals['date_start'] = ds.replace(day=1)
             except Exception:
                 pass
-        if vals.get('next_invoice_date'):
+        subscription = super().create(vals)
+        # عدل next_invoice_date بعد الإنشاء
+        if subscription.next_invoice_date:
             try:
-                nd = fields.Date.from_string(vals['next_invoice_date'])
-                vals['next_invoice_date'] = nd.replace(day=1)
+                nd = fields.Date.from_string(subscription.next_invoice_date)
+                subscription.next_invoice_date = nd.replace(day=1)
             except Exception:
                 pass
-        return super().create(vals)
+        return subscription
 
     def write(self, vals):
-        # تأكد أن date_start و next_invoice_date في أول الشهر
-        if vals.get('date_start'):
-            try:
-                ds = fields.Date.from_string(vals['date_start'])
-                vals['date_start'] = ds.replace(day=1)
-            except Exception:
-                pass
-        if vals.get('next_invoice_date'):
-            try:
-                nd = fields.Date.from_string(vals['next_invoice_date'])
-                vals['next_invoice_date'] = nd.replace(day=1)
-            except Exception:
-                pass
-        return super().write(vals)
+        res = super().write(vals)
+        for sub in self:
+            if sub.date_start:
+                try:
+                    ds = fields.Date.from_string(sub.date_start)
+                    sub.date_start = ds.replace(day=1)
+                except Exception:
+                    pass
+            if sub.next_invoice_date:
+                try:
+                    nd = fields.Date.from_string(sub.next_invoice_date)
+                    sub.next_invoice_date = nd.replace(day=1)
+                except Exception:
+                    pass
+        return res
