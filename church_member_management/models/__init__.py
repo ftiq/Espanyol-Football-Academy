76dc94228c161ae446c7f91df306a5ca8d8ea9a7
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-20 17:55:37 +0300

    Add files via upload

---
+++ church_member_management/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+# This line imports the 'res_partner.py' file.
+# This makes the ResPartner class (and its modifications) available to Odoo.
+from . import cmm_member
+from . import cmm_ministry
+from . import res_partner
