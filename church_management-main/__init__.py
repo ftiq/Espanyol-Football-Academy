fe9db4c6c99ae684509363dfff454e813cce5880
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-20 17:45:38 +0300

    Delete church_management-main directory

--- church_management-main/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# This line imports the 'models' subdirectory.
-# When Odoo loads your addon, it will execute this __init__.py file,
-# which in turn will execute the __init__.py file inside the 'models' directory.
-from . import models
--- church_management-main/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# This line imports the 'res_partner.py' file.
-# This makes the ResPartner class (and its modifications) available to Odoo.
-from . import cmm_member
-from . import cmm_ministry
-from . import res_partner
